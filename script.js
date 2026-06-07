01dd85fb2835961b848df0cfc40e936ecceb1752
samtan <samtangroup@gmail.com>
2026-06-07 18:39:27 +0530

    updated new code

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+const counters=document.querySelectorAll('.counter');
+
+counters.forEach(counter=>{
+
+const updateCounter=()=>{
+
+const target=+counter.getAttribute('data-target');
+const count=+counter.innerText;
+
+const increment=target/100;
+
+if(count<target){
+
+counter.innerText=Math.ceil(count+increment);
+
+setTimeout(updateCounter,20);
+
+}else{
+
+counter.innerText=target;
+
+}
+
+};
+
+updateCounter();
+
+});
+
 window.addEventListener("load", () => {
 
 const counters = document.querySelectorAll(".counter");
@@ -12,7 +41,7 @@ const updateCounter = () => {
 
 const increment = target / 50;
 
-if (count < target) {
+if(count < target){
 
 count += increment;
 
@@ -20,7 +49,7 @@ counter.innerText = Math.ceil(count);
 
 requestAnimationFrame(updateCounter);
 
-} else {
+}else{
 
 counter.innerText = target + "+";
 
@@ -29,6 +58,18 @@ counter.innerText = target + "+";
 };
 
 updateCounter();
+
+});
+
+});
+
+document.querySelectorAll(".faq-question")
+.forEach(question=>{
+
+question.addEventListener("click",()=>{
+
+question.parentElement
+.classList.toggle("active");
 
 });
 
